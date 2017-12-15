a045fd7f899c4662c808ce4216040d8773057f48
znlccy <756311868@qq.com>
2017-12-15 17:53:03 +0800

    添加登录界面

--- app/src/main/java/znlccy/com/electronicemporium/Welcome.java
+++ app/src/main/java/znlccy/com/electronicemporium/Welcome.java
@@ -3,15 +3,29 @@ package znlccy.com.electronicemporium;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class Welcome extends AppCompatActivity {
 
+    /*声明打开第二个activity窗口*/
     private Button openIndexBtn;
 
-    private ImageButton imageOpenBtn;
+    /*声明登录按钮*/
+    private Button loginBtn;
+
+    /*声明注册按钮*/
+    private Button registeredBtn;
+
+    /*声明用户名编辑框*/
+    private EditText username;
+
+    /*声明密码编辑框*/
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,21 +36,34 @@ public class Welcome extends AppCompatActivity {
         openIndexBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Welcome.this,SettingsActivity.class);
-                intent.putExtra("name","chencongye");
-                startActivity(intent);
-            }
-        });
-
-        imageOpenBtn = (ImageButton) findViewById(R.id.imagebtn_open);
-        imageOpenBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
                 Intent intent = new Intent(Welcome.this,Index.class);
                 intent.putExtra("name","chencongye");
                 startActivity(intent);
             }
         });
+
+       loginBtn = (Button) findViewById(R.id.login_id);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*Intent intent = new Intent(Welcome.this,Index.class);
+                intent.putExtra("username","chencongye");*/
+            }
+        });
+
+        registeredBtn = (Button) findViewById(R.id.registered_id);
+        registeredBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Welcome.this,Registered.class);
+                startActivity(intent);
+
+            }
+        });
+        password = (EditText) findViewById(R.id.userpassword_id);
+        username = (EditText) findViewById(R.id.username_id);
+        password.setText(username.getText().toString());
     }
 
     public void sendJsonData() {
